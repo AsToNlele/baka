e4308e44d8e65cdfb38e651226e19982f910c6b3
AsToNlele <theastonofficial@gmail.com>
2024-03-29 16:26:24 +0100

    fix: Order permissions

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -8,22 +8,37 @@ from orders.serializers import (
     PaymentSerializer,
     ProductOrderSerializer,
 )
-from rest_framework import viewsets
-from rest_framework.decorators import action
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action, permission_classes
 from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.views import Response
+
+
+class IsOrderOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user.profile
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    # permission_classes = [IsAuthenticated]
 
-    # def get_serializer_class(self):
-    #     if hasattr(self, 'action') and self.action == 'list':
-    #         return OrderSerializer
+    def get_permissions(self):
+        if self.action == "create":
+            permission_classes = [IsAuthenticated]
+        elif self.action == "list":
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [IsOrderOwner]
+        return [permission() for permission in permission_classes]
 
     def list(self, request):
-        queryset = Order.objects.all()
+        # queryset only owned
+        queryset = Order.objects.filter(user=request.user.profile)
         serializer = OrderSerializer(queryset, many=True)
         print(serializer.data)
         orders = []
@@ -42,8 +57,14 @@ class OrderViewSet(viewsets.ModelViewSet):
         return Response(orders)
 
     def retrieve(self, request, pk=None):
-        queryset = Order.objects.all()
-        order = get_object_or_404(queryset, pk=pk)
+        # queryset = Order.objects.all()
+        # order = get_object_or_404(queryset, pk=pk)
+        order = self.get_object()
+        # permission_classes = [IsOrderOwner | IsAdminUser]
+        # # Only if owner
+        # if order.user != request.user.profile:
+        #     return JsonResponse({"error": "Unauthorized"}, status=401)
+
         if hasattr(order, "flowerbedorders"):
             flowerbed_order = FlowerbedOrders.objects.get(id=pk)
             flowerbed_order_serializer = FlowerbedOrderSerializer(flowerbed_order)
@@ -55,7 +76,8 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["get"], serializer_class=PaymentSerializer)
     def get_payment(self, request, pk=None):
-        order = Order.objects.get(id=pk)
+        order = self.get_object()
+
         if order.status != "created":
             return Response({"error": "Order is already paid"})
         bankAccountNumber = "2102758516/2010"
@@ -73,12 +95,15 @@ class OrderViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=["get"], serializer_class=GetPickupLocationsSerializer)
     def get_pickup(self, request, pk=None):
+        order = self.get_object()
         queryset = ProductOrders.objects.all()
         productOrder = get_object_or_404(queryset, pk=pk)
 
+        if productOrder.user != request.user.profile:
+            return JsonResponse({"error": "Unauthorized"}, status=401)
+
         if not productOrder:
             return Response({"error": "Order not found"})
-
 
         class GreenhouseWithItems:
             def __init__(self, greenhouse):
@@ -90,7 +115,6 @@ class OrderViewSet(viewsets.ModelViewSet):
 
             def __str__(self):
                 return f"{self.greenhouse.title} - {', '.join([item.productName for item in self.items])}"
-                
 
         greenhousesWithItems = []
 
@@ -99,7 +123,7 @@ class OrderViewSet(viewsets.ModelViewSet):
         for item in items:
             greenhouse = Greenhouse.objects.get(id=item.greenhouseId)
             greenhouseWithItems = GreenhouseWithItems(greenhouse)
-            
+
             isInList = False
 
             for ghi in greenhousesWithItems:
@@ -113,13 +137,10 @@ class OrderViewSet(viewsets.ModelViewSet):
                 greenhousesWithItems.append(greenhouseWithItems)
 
         for greenhouseWithItems in greenhousesWithItems:
-            print(len( greenhousesWithItems ))
+            print(len(greenhousesWithItems))
             print(greenhouseWithItems)
 
-        
         serializer = GetPickupLocationsSerializer(data=greenhousesWithItems, many=True)
         serializer.is_valid()
 
         return Response(serializer.data)
-        
-
