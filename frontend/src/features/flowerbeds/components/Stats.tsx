6273ce35c34bc45772574e7d00314b1f801578e3
AsToNlele <theastonofficial@gmail.com>
2024-04-21 00:27:52 +0200

    fix: import

---
+++ frontend/src/features/flowerbeds/components/Stats.tsx
@@ -0,0 +1,10 @@
+// import { useHarvestStore } from "@/features/flowerbeds/stores/useHarvestStore"
+
+export const Stats = () => {
+    // const { localHarvests } = useHarvestStore()
+    return (
+        <div>
+            <h1 className="mb-2 text-lg font-semibold">Statistics</h1>
+        </div>
+    )
+}
